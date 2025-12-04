170b4232f3e68d3b393a2c1958b84137bab95d1a
volrriti <khandaaym47@gmail.com>
2025-12-04 09:44:56 +0545

    dsfs

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export", // enables static HTML export
+  basePath: "/portfolio", // sets the base path for the application
 };
 
 export default nextConfig;
